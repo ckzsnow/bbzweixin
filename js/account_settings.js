d6c6e8b6431bb13c5d237f61b79dda799d36393c
brook <Brookchai@bangbangzhang.com>
2015-12-22 19:07:15 +0800

    modify

---
+++ js/account_settings.js
@@ -0,0 +1,152 @@
+mui.createProcessingMask = function(callback) {
+	var element = document.createElement('div');
+	element.classList.add('upload-file');
+	element.addEventListener('touchmove', mui.preventDefault);
+	element.addEventListener('tap', function() {
+		//mask.close();
+	});
+	var processingNode = document.createElement('div');
+	processingNode.setAttribute('class', 'mui-loading');
+	processingNode.innerHTML = "<div class='mui-spinner' style='width:60px;height:60px;'></div><div style='text-align: center;color:white;'>正在处理请求</div>";
+	element.appendChild(processingNode);
+	var mask = [element];
+	mask._show = false;
+	mask.show = function() {
+		mask._show = true;
+		element.setAttribute('style', 'opacity:1');
+		//processingNode.style.marginLeft = window.screen.availWidth/2 + "px";
+		processingNode.style.marginTop = window.screen.availHeight / 2 - 30 + "px";
+		document.body.appendChild(element);
+		return mask;
+	};
+	mask._remove = function() {
+		if (mask._show) {
+			mask._show = false;
+			element.setAttribute('style', 'opacity:0');
+			mui.later(function() {
+				var body = document.body;
+				element.parentNode === body && body.removeChild(element);
+			}, 350);
+		}
+		return mask;
+	};
+	mask.close = function() {
+		if (callback) {
+			if (callback() !== false) {
+				mask._remove();
+			}
+		} else {
+			mask._remove();
+		}
+	};
+	return mask;
+};
+
+mui.createTipDialog = function(info, callBack) {
+	var template = "<div style='width:80%;margin:50% 10%;border:1px solid #ddd;background-color: white;border-radius: 5px;'><div style='margin-top:20px;margin-left:20px;'>提示信息</div><hr/><div style='margin-top:20px;margin-left:20px;margin-bottom:20px;margin-right:20px;height:60px;'>{{info}}</div></div>";
+	var element = document.createElement('div');
+	element.classList.add('dialog');
+	element.innerHTML = template.replace('{{info}}', info);
+	element.addEventListener('touchmove', mui.preventDefault);
+	element.addEventListener('tap', function() {
+		mask.close();
+	});
+	var mask = [element];
+	mask._show = false;
+	mask.show = function() {
+		mask._show = true;
+		element.setAttribute('style', 'opacity:1');
+		document.body.appendChild(element);
+		return mask;
+	};
+	mask._remove = function() {
+		if (mask._show) {
+			mask._show = false;
+			element.setAttribute('style', 'opacity:0');
+			mui.later(function() {
+				var body = document.body;
+				element.parentNode === body && body.removeChild(element);
+			}, 350);
+		}
+		return mask;
+	};
+	mask.close = function() {
+		if (callBack) {
+			callBack();
+		}
+		mask._remove();
+	};
+	return mask;
+};
+
+mui.createConfirmDialog = function(info, cancelCallBack, acceptCallBack) {
+	var template = "<div style='width:80%;margin:50% 10%;border:1px solid #ddd;background-color: white;border-radius: 5px;'><div style='margin-top:20px;margin-left:20px;'>提示信息</div><hr/><div style='margin-top:20px;margin-left:20px;margin-bottom:20px;margin-right:20px;height:60px;'>{{info}}</div><div style='text-align:right;margin-bottom:20px;margin-right:20px;'><a id='createConfirmDialog_cancel' href='javascript:void(0);' style='margin-right:20px;text-decoration:none;'>取消</a><a id='createConfirmDialog_accept' href='javascript:void(0);' style='text-decoration:none;'>确定</a></div></div>";
+	var element = document.createElement('div');
+	element.classList.add('dialog');
+	element.innerHTML = template.replace('{{info}}', info);
+	element.addEventListener('touchmove', mui.preventDefault);
+	var mask = [element];
+	mask._show = false;
+	mask.show = function() {
+		mask._show = true;
+		element.setAttribute('style', 'opacity:1');
+		document.body.appendChild(element);
+		document.getElementById('createConfirmDialog_cancel').addEventListener('tap', function() {
+			if (cancelCallBack) cancelCallBack();
+			mask.close();
+		});
+		document.getElementById('createConfirmDialog_accept').addEventListener('tap', function() {
+			if (acceptCallBack) acceptCallBack();
+			mask.close();
+		});
+		return mask;
+	};
+	mask._remove = function() {
+		if (mask._show) {
+			mask._show = false;
+			element.setAttribute('style', 'opacity:0');
+			mui.later(function() {
+				var body = document.body;
+				element.parentNode === body && body.removeChild(element);
+			}, 350);
+		}
+		return mask;
+	};
+	mask.close = function() {
+		mask._remove();
+	};
+	return mask;
+};
+
+mui.init();
+mui('.mui-scroll-wrapper').scroll();
+
+function bindCompany() {
+	var mask = mui.createProcessingMask(null);
+	mask.show();
+	mui.ajax({
+		url: '/approvalBill',
+		type: "POST",
+		data: {
+			
+		},
+		success: function(data) {
+			mui.createTipDialog('绑定公司成功！',null).show();
+			mask.close();
+		},
+		error: function(status, error) {
+			mask.close();
+			mui.createTipDialog('服务器暂时无法响应您的请求，请稍后重试！',null).show();
+		}
+	});
+}
+
+function unBindCompany() {
+	var mask = mui.createProcessingMask(null);
+	mask.show();
+}
+
+function saveInfo() {
+	var mask = mui.createProcessingMask(null);
+	mask.show();
+}
